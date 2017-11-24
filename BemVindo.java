7f082f8dca324d0ae31d6f4afddfb83773bf7c9a
LMFM2002 <32169116+LMFM2002@users.noreply.github.com>
2017-11-24 15:06:14 +0000

    Add files via upload

---
+++ BemVindo.java
@@ -0,0 +1,9 @@
+
+public class BemVindo {
+
+	public static void main(String[] args) {		
+	 String message = ("Hoje é sexta feira");
+		for(int I=1;I  <=5; I++);
+		System.out.println(message);
+		}
+	}
