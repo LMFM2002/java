801b2e3a9da193d1d36d4ac32a4d39fa5c5f5345
LMFM2002 <32169116+LMFM2002@users.noreply.github.com>
2017-11-24 15:53:00 +0000

    Add files via upload

---
+++ Soma1.java
@@ -0,0 +1,16 @@
+
+public class Soma1 {
+
+	public static void main(String[] args) {
+		Scanner 1 = new Scanner (System.in);
+		int soma=0;
+		int num;
+		System.out.print("Qual o numero a introduzir);
+		 num = I.nextInt();
+		for(int I=1 ; I<=10 ; I++);
+		  soma +=I;
+	      System.out.println("O total é "+soma);
+	      System.out.println(soma);
+		
+	}
+}
